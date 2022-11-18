fe450cb68aaf47fa36bb5b2322b9ad878c1a1aba
Gus Hogg-Blake <gus@gushogg-blake.com>
2022-11-18 12:27:18 +0000

    wip

--- src/components/FindAndReplace.svelte
+++ src/components/FindAndReplace.svelte
@@ -414,7 +414,7 @@ onMount(function() {
 				<option value="files">Files</option>
 			</select>
 			<input bind:value={formOptions.paths} id="paths" disabled={formOptions.searchIn !== "files"}>
-			<Checkbox bind:value={formOptions.searchInSubDirs} label="Search in su%b directories" disabled={formOptions.searchIn !== "files"}/>
+			<Checkbox bind:value={formOptions.searchInSubDirs} label="Search su%b dirs" disabled={formOptions.searchIn !== "files"}/>
 		</div>
 		<AccelLabel for="include" label="Incl%ude" disabled={formOptions.searchIn !== "files"}/>
 		<div class="input">
