ea041fe13ff6e9030e859a615aa53deb1fd5c230
Gus Hogg-Blake <gus@gushogg-blake.com>
2022-12-22 17:14:32 +0000

    wip

---
+++ src/modules/codex/replace/getEditToReplaceResult.js
@@ -0,0 +1,9 @@
+/*
+accepts a document, a codex find result, and an array of
+{lineTuples, placeholders} objects describing the replacement,
+and returns an edit that will perform the replacement
+*/
+
+module.exports = function(document, result, replaceWithLineTuplesWithPlaceholders) {
+	
+}
