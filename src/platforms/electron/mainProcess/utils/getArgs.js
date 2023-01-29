9c684f6dcb130020aaa782e588615f2bdca64b73
Gus Hogg-Blake <gus@gushogg-blake.com>
2023-01-29 10:25:12 +0000

    wip

--- src/platforms/electron/mainProcess/utils/getArgs.js
+++ src/platforms/electron/mainProcess/utils/getArgs.js
@@ -1,13 +1,24 @@
-let {hideBin} = require("yargs/helpers");
 let yargs = require("yargs/yargs");
 
+/*
+args may or may not have node/electron at the beginning, and the
+first "." could be from "electron ."
+*/
+
 module.exports = function(argv) {
-	let start = argv.indexOf("--begin-app-args");
-	let end = argv.indexOf("--end-app-args");
+	let start = argv.indexOf("--start-args=1");
 	
-	if (start !== -1 && end !== -1) {
-		argv = argv.slice(start + 1, end);
+	if (start === -1) {
+		throw new Error("--start-args=1 argument required to separate command-line args from node/electron paths");
 	}
 	
-	return yargs(hideBin(argv));
+	argv = argv.slice(start + 1);
+	
+	let dot = argv.indexOf(".");
+	
+	if (dot !== -1) {
+		argv.splice(dot, 1);
+	}
+	
+	return yargs(argv);
 }
