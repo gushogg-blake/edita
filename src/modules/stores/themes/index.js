379f23d4ead7092dc9a3b7b13735dc7af82a5809
Gus Hogg-Blake <gus@gushogg-blake.com>
2023-06-30 22:24:36 +0100

    migrate

--- src/modules/stores/themes/index.js
+++ src/modules/stores/themes/index.js
@@ -3,7 +3,7 @@ let JsonStore = require("modules/JsonStore");
 let defaultThemes = require("./defaultThemes");
 
 let migrations = {
-	"11"(theme, key) {
+	"12"(theme, key) {
 		return defaultThemes[key] || undefined;
 	},
 };
