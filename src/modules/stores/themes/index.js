2c897408b843d063388490ff301fad95da9e6cbc
Gus Hogg-Blake <gus@gushogg-blake.com>
2024-11-29 15:54:39 +0000

    wip

--- src/modules/stores/themes/index.js
+++ src/modules/stores/themes/index.js
@@ -3,7 +3,7 @@ let JsonStore = require("modules/JsonStore");
 let defaultThemes = require("./defaultThemes");
 
 let migrations = {
-	"13"(theme, key) {
+	"16"(theme, key) {
 		return defaultThemes[key] || undefined;
 	},
 };
