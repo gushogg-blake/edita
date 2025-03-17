17b8e952ee6b25980483fdcbf0fe08a2e98a9bb3
Gus Hogg-Blake <gus@gushogg-blake.com>
2025-03-17 18:03:35 +0000

    fix

--- src/platforms/electron/modules/ipc/utils/handleMessages.ts
+++ src/platforms/electron/modules/ipc/utils/handleMessages.ts
@@ -1,7 +1,7 @@
 import ipcRenderer from "platforms/electron/modules/ipcRenderer";
 
 export default function(channel, handler) {
-	return ipcRenderer.on(channel, function(e, method, ...args) {
+	return ipcRenderer.handle(channel, function(e, method, ...args) {
 		return handler[method](e, ...args);
 	});
 }
