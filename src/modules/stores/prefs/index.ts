41ff6f49d1d74fb6ac1de42d7a1e926a7fe07d93
Gus Hogg-Blake <gus@gushogg-blake.com>
2025-03-17 22:38:40 +0000

    split migrations out of prefs store

--- src/modules/stores/prefs/index.ts
+++ src/modules/stores/prefs/index.ts
@@ -1,226 +1,5 @@
 import JsonStore from "modules/JsonStore";
-
-let migrations = {
-	"1"(prefs) {
-		prefs.astKeymap["c"] = "change";
-	},
-	
-	"2"(prefs) {
-		prefs.fontFamily = prefs.font;
-		
-		delete prefs.font;
-	},
-	
-	"3"(prefs) {
-		delete prefs.fontFamily;
-		delete prefs.fontSize;
-		
-		delete prefs.lineNumberColor;
-		delete prefs.marginBackground;
-		delete prefs.selectionBackground;
-		delete prefs.hiliteBackground;
-		delete prefs.astSelectionBackground;
-		delete prefs.astSelectionHiliteBackground;
-		delete prefs.astInsertionHiliteBackground;
-		delete prefs.foldHeaderBackground;
-		delete prefs.foldHeaderBorder;
-		
-		delete prefs.langs;
-		
-		prefs.theme = "light";
-	},
-	
-	"4"(prefs) {
-		prefs.showThemeSelector = false;
-	},
-	
-	"5"(prefs) {
-		prefs.astKeymap["f"] = "toggleMultiline";
-	},
-	
-	"6"(prefs) {
-		delete prefs.astKeymap["c"];
-		delete prefs.astKeymap["f"];
-		delete prefs.astKeymap["w"];
-		delete prefs.astKeymap["u"];
-		
-		prefs.astManipulationKeymap = {
-			common: {
-				"c": "$change",
-				
-				"f": "toggleMultilineOuter",
-				"g": "toggleMultilineInner",
-				
-				"w": "wrap",
-				"u": "unwrap",
-			},
-		};
-	},
-	
-	"7"(prefs) {
-		prefs.maxLineLengthForParsing = 300;
-	},
-	
-	"8"(prefs) {
-		prefs.normalKeymap[""] = "pasteAndIndent";
-	},
-	
-	"9"(prefs) {
-		prefs.normalKeymap["Ctrl+Shift+Space"] = "completeWordPrevious";
-	},
-	
-	"10"(prefs) {
-		delete prefs.normalKeymap[""];
-		
-		prefs.normalKeymap["Ctrl+Shift+V"] = "pasteAndIndent";
-	},
-	
-	"11"(prefs) {
-		delete prefs.maxLineLengthForParsing;
-	},
-	
-	"12"(prefs) {
-		prefs.dev = {
-			showToolbar: false,
-			timing: {},
-			showThemeStyleElement: false,
-		};
-	},
-	
-	"13"(prefs) {
-		prefs.dev = {
-			showToolbar: false,
-			timing: {},
-			showThemeStyleElement: false,
-			openFindAndReplace: false,
-			logFocusedElement: false,
-		};
-	},
-	
-	"14"(prefs) {
-		prefs.dev.openRefactor = false;
-	},
-	
-	"15"(prefs) {
-		delete prefs.panes.bottom;
-		
-		prefs.panes.bottom1 = {
-			visible: false,
-			size: 500,
-		};
-			
-		prefs.panes.bottom2 = {
-			visible: false,
-			size: 240,
-		};	
-	},
-	
-	"16"(prefs) {
-		delete prefs.panes.bottom1;
-		delete prefs.panes.bottom2;
-		
-		prefs.panes.bottom = {
-			preferredSizes: {
-				totalWithTopExpanded: 500,
-				bottomContents: 200,
-			},
-			
-			top: {
-				visible: true,
-				expanded: false,
-			},
-			
-			bottom: {
-				visible: true,
-				expanded: true,
-			},
-		};
-	},
-	
-	"17"(prefs) {
-		delete prefs.panes.bottom;
-		
-		prefs.panes.bottom = {
-			preferredSizes: {
-				totalWithTopExpanded: 500,
-				bottomContents: 200,
-			},
-			
-			top: {
-				visible: true,
-				expanded: false,
-			},
-			
-			bottom: {
-				visible: true,
-				expanded: true,
-			},
-		};
-	},
-	
-	"18"(prefs) {
-		prefs.panes.bottom = {
-			preferredSizes: {
-				totalWithTopExpanded: 500,
-				bottomContentsWithTop: 120,
-				bottomContentsWithoutTop: 200,
-			},
-			
-			top: {
-				visible: true,
-				expanded: false,
-			},
-			
-			bottom: {
-				visible: true,
-				expanded: true,
-			},
-		};
-	},
-	
-	"19"(prefs) {
-		prefs.panes.left.visible = false;
-		prefs.panes.right.visible = false;
-		prefs.panes.bottom.top.visible = false;
-		prefs.panes.bottom.bottom.visible = false;
-	},
-	
-	"20"(prefs) {
-		prefs.insertNestedSnippets = "blankLines";
-	},
-	
-	"21"(prefs) {
-		prefs.ctrlScrollMultiplier = 5;
-		
-		delete prefs.editorMouseMap["Ctrl+Wheel"];
-	},
-	
-	"22"(prefs) {
-		prefs.globalKeymap["Ctrl+L"] = "newWithLangSelector";
-	},
-	
-	"25"(prefs) {
-		prefs.defaultWrapLangs = ["markdown"];
-	},
-	
-	"26"(prefs) {
-		prefs.globalKeymap["Ctrl+Shift+D"] = "toggleDevToolbar";
-	},
-	
-	"27"(prefs) {
-		prefs.normalKeymap["Ctrl+End"] = "end";
-		prefs.normalKeymap["Ctrl+Home"] = "home";
-	},
-	
-	"28"(prefs) {
-		prefs.normalKeymap["Ctrl+ArrowUp"] = "up";
-		prefs.normalKeymap["Ctrl+ArrowDown"] = "down";
-	},
-	
-	"29"(prefs) {
-		prefs.globalKeymap["Ctrl+#"] = "focusEditor";
-	},
-};
+import migrations from "./migrations";
 
 export default function() {
 	let defaultPrefs = {
@@ -231,15 +10,16 @@ export default function() {
 		defaultWrapLangs: ["markdown"],
 		
 		wrap: false,
+		copyLineIfSelectionNotFull: false,
 		
 		modeSwitchKey: "Escape",
 		minHoldTime: 200,
 		
+		customContextMenu: true,
+		
 		zoom: {
 			stopAtProjectRoot: true,
 		},
-		
-		copyLineIfSelectionNotFull: false,
 		
 		verticalSpacing: {
 			spaceBlocks: true,
@@ -396,7 +176,6 @@ export default function() {
 		
 		fileAssociations: {
 			"html": ["*.svelte"],
-			//"plaintext": ["*.js"],
 		},
 		
 		theme: "light",
