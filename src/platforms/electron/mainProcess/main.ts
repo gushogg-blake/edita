32098d8b68cc2a080d2ddae4661437c551c3befd
Gus Hogg-Blake <gus@gushogg-blake.com>
2025-03-17 11:21:55 +0000

    remove printr from electron main

--- src/platforms/electron/mainProcess/main.ts
+++ src/platforms/electron/mainProcess/main.ts
@@ -9,50 +9,6 @@ import watch from "./watch";
 
 electronApp.setPath("userData", path.join(config.userDataDir, "electron"));
 
-let {debugEndpoint} = config;
-
-if (debugEndpoint) {
-	function printr(data, stack) {
-		let curl = spawn("curl", [
-			"-H", "Content-Type: application/json",
-			"--data-binary", "@-",
-			debugEndpoint,
-		], {
-			stdio: ["pipe", "ignore", "ignore"],
-		});
-		
-		curl.stdin.write(JSON.stringify({
-			stack,
-			data,
-		}));
-		
-		curl.stdin.end();
-	}
-	
-	let oldOut = process.stdout.write;
-	let oldErr = process.stderr.write;
-	
-	process.stdout.write = function(str, enc, fd) {
-		let e = {};
-		
-		Error.captureStackTrace(e, process.stdout.write);
-		
-		printr(str, e.stack);
-		
-		oldOut.call(process.stdout, str, enc, fd);
-	}
-	
-	process.stderr.write = function(str, enc, fd) {
-		let e = {};
-		
-		Error.captureStackTrace(e, process.stderr.write);
-		
-		printr(str, e.stack);
-		
-		oldErr.call(process.stderr, str, enc, fd);
-	}
-}
-
 (async function() {
 	let app = new App();
 	
