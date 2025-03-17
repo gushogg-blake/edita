01570af32a51ca3424c5eacf664a1686d4e7938e
Gus Hogg-Blake <gus@gushogg-blake.com>
2025-03-17 22:37:27 +0000

    update HACK note about using html grammar for svelte - still needed

--- src/components/App/Toolbar.svelte
+++ src/components/App/Toolbar.svelte
@@ -23,11 +23,11 @@ function openLanguages() {
 			async onClick() {
 				let tab = await app.newFile(lang);
 				
-				// TODO see if this is still necessary
 				// HACK - Svelte grammar is broken so we actually want
 				// HTML (HTML lang module supports .svelte)
 				// so we actually just want whatever lang the extension
 				// indicates
+				// TODO refactor
 				
 				await tab.document.updateFormat();
 			},
