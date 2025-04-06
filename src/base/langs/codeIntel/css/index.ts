ec5f199a8b63bbb8f063def6a26f46245a11427f
Gus Hogg-Blake <gus@gushogg-blake.com>
2025-04-06 16:17:02 +0100

    wip

--- src/base/langs/codeIntel/css/index.ts
+++ src/base/langs/codeIntel/css/index.ts
@@ -1,6 +1,24 @@
 import {CodeIntel} from "modules/codeIntel";
 
 export default class extends CodeIntel {
+	commentLines(document, startLineIndex, endLineIndex) {
+		let lines = document.lines.slice(startLineIndex, endLineIndex);
+		let minIndentLevel = Math.min(...lines.map(line => line.indentLevel));
+		let minIndent = document.format.indentation.string.repeat(minIndentLevel);
+		
+		return lines.map(function(line) {
+			return line.string.replace(new RegExp("^" + minIndent), minIndent + "/*") + "*/";
+		}).join(document.format.newline);
+	}
+	
+	uncommentLines(document, startLineIndex, endLineIndex) {
+		let lines = document.lines.slice(startLineIndex, endLineIndex);
+		
+		return lines.map(function(line) {
+			return line.string.replace(/^(\s*)\/\*/, "$1").replace(/\*\/$/, "");
+		}).join(document.format.newline);
+	}
+	
 	indentOnNewline(document, line, cursor) {
 		return line.string.substr(0, cursor.offset).match(/[\[{(]$/);
 	}
--- src/base/langs/core/ruby/index.ts
+++ src/base/langs/core/ruby/index.ts
@@ -6,24 +6,6 @@ export default class extends Lang {
 	defaultExtension = "rb";
 	injections = [];
 	
-	commentLines(document, startLineIndex, endLineIndex) {
-		let lines = document.lines.slice(startLineIndex, endLineIndex);
-		let minIndentLevel = Math.min(...lines.map(line => line.indentLevel));
-		let minIndent = document.format.indentation.string.repeat(minIndentLevel);
-		
-		return lines.map(function(line) {
-			return line.string.replace(new RegExp("^" + minIndent), minIndent + "//");
-		}).join(document.format.newline);
-	}
-	
-	uncommentLines(document, startLineIndex, endLineIndex) {
-		let lines = document.lines.slice(startLineIndex, endLineIndex);
-		
-		return lines.map(function(line) {
-			return line.string.replace(/^(\s*)(\/\/)?/, "$1");
-		}).join(document.format.newline);
-	}
-	
 	getSupportLevel(code, path) {
 		if (!path) {
 			return null; //
