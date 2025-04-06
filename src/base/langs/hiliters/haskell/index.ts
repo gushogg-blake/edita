bf53bcd004fa113e6514689a7b651206e104206e
Gus Hogg-Blake <gus@gushogg-blake.com>
2025-04-06 16:11:43 +0100

    wip

--- src/base/langs/hiliters/haskell/index.ts
+++ src/base/langs/hiliters/haskell/index.ts
@@ -95,22 +95,4 @@ export default class extends Hiliter {
 		
 		return "symbol";
 	}
-	
-	commentLines(document, startLineIndex, endLineIndex) {
-		let lines = document.lines.slice(startLineIndex, endLineIndex);
-		let minIndentLevel = Math.min(...lines.map(line => line.indentLevel));
-		let minIndent = document.format.indentation.string.repeat(minIndentLevel);
-		
-		return lines.map(function(line) {
-			return line.string.replace(new RegExp("^" + minIndent), minIndent + "//");
-		}).join(document.format.newline);
-	}
-	
-	uncommentLines(document, startLineIndex, endLineIndex) {
-		let lines = document.lines.slice(startLineIndex, endLineIndex);
-		
-		return lines.map(function(line) {
-			return line.string.replace(/^(\s*)(\/\/)?/, "$1");
-		}).join(document.format.newline);
-	}
 }
--- src/base/langs/hiliters/markdown_inline/index.ts
+++ src/base/langs/hiliters/markdown_inline/index.ts
@@ -4,14 +4,6 @@ import {Hiliter} from "modules/hiliter";
 export default class extends Hiliter {
 	 // to get snippets
 	
-	getFooter(node) {
-		return null;
-	}
-	
-	getHeader(node) {
-		return null;
-	}
-	
 	getHiliteClass(node: Node): string | null {
 		let {
 			type,
@@ -26,23 +18,5 @@ export default class extends Hiliter {
 		}
 		
 		return null;
-	}
-	
-	commentLines(document, startLineIndex, endLineIndex) {
-		let lines = document.lines.slice(startLineIndex, endLineIndex);
-		let minIndentLevel = Math.min(...lines.map(line => line.indentLevel));
-		let minIndent = document.format.indentation.string.repeat(minIndentLevel);
-		
-		return lines.map(function(line) {
-			return line.string.replace(new RegExp("^" + minIndent), minIndent + "<!--") + "-->";
-		}).join(document.format.newline);
-	}
-	
-	uncommentLines(document, startLineIndex, endLineIndex) {
-		let lines = document.lines.slice(startLineIndex, endLineIndex);
-		
-		return lines.map(function(line) {
-			return line.string.replace(/^(\s*)(<!--)?/, "$1").replace(/-->$/, "");
-		}).join(document.format.newline);
 	}
 }
